4e1618a51f939c051fd3ecc9dd8f5adb53c196ee
Noah Fruttarol <noah.fruttarol@ec.gc.ca>
2026-02-26 21:29:51 +0000

    created files for MIDAS CMORization

---
+++ esmvaltool/cmorizers/data/formatters/datasets/midas.py
@@ -0,0 +1,11 @@
+"""ESMValTool cmorizer for Midas data.
+
+Tier
+    Tier 2
+
+Source
+    https://zenodo.org/records/4244106#.Y5ekg3bMKMo
+
+Modification history:
+    20260226-Fruttarol_Noah: Written
+"""
